5e898bb8129ccd14de1ebb58ea67f0996ea4036b
Annie <anniewang@softmobile.com.tw>
2022-09-05 14:18:43 +0800

    TS-Learn

--- typescript/src/class.ts
+++ typescript/src/class.ts
@@ -1,17 +1,20 @@
 class Book {
 	public bookName: string;	// public：為預設，可不寫
-	private price: number;		// private：私有屬性
+	private author: string;		// private：私有屬性
+	protected price: number;	// Protected：子類別無法使用父類別的private，protected可授權子類別使用，又能保有私有性質
 
-	constructor(name: string, price: number){	// 初始化
+	// 初始化
+	constructor(name: string, author: string, price: number){
 		this.bookName = name;
+		this.author = author;
 		this.price = price;
 	}
 
 	showInfo(this: Book){
-		console.log("書名：" + this.bookName + "\n價格：$" + this.price);
+		console.log("書名：" + this.bookName + "\n作者：" + this.author + "\n價格：$" + this.price);
 	}
 }
-const newbook = new Book('子兒吐吐', 399);
+const newbook = new Book('子兒吐吐', '無名', 399);
 newbook.showInfo();
 
 // ---------------------------------------------------------------------------------------------------------
@@ -65,6 +68,7 @@ class professionalPlayer extends Player{
 	constructor(uuID: string, public seniority: string[]){
 		super(uuID, "職業玩家");
 	}
+	// 功能擴充
 	showTrophy() {
 		console.log("資歷：" + this.seniority);
 	}
@@ -74,3 +78,18 @@ NewVIPPlayer.showInfo();
 const NewprofessionalPlayer = new professionalPlayer("09-22113344", ['Apex：賽季2022-1 #3','小朋友下樓梯：20118層','...']);
 NewprofessionalPlayer.showInfo();
 NewprofessionalPlayer.showTrophy();
+
+// ---------------------------------------------------------------------------------------------------------
+// Protected 子類別無法使用父類別的private，protected可授權子類別使用，又能保有私有性質
+class SaleBook extends Book{
+	constructor(){
+		super('企鵝家族', '軼名', 500);
+	}
+	sale(discount: number){
+		this.price *= discount
+	}
+}
+const NewSaleBook = new SaleBook();
+NewSaleBook.showInfo();
+NewSaleBook.sale(0.7);
+NewSaleBook.showInfo();
