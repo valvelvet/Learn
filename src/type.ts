c87e5ebe83bf929f270a0040ab520481880a1c4a
Annie <anniewang@softmobile.com.tw>
2022-10-19 11:19:15 +0800

    Learn-TS-update mindMap

--- src/type.ts
+++ src/type.ts
@@ -15,43 +15,59 @@
 // Tuple		[number, string]
 const book = {
   ISBN: 100098,
-  name: 'Read me now!',
+  name: "Read me now!",
   onSale: true,
   saleInfo: {
     prise: 399,
     inventory: 37,
   },
-  class: ['Horror', 'mystery'],
+  class: ["Horror", "mystery"],
   menu: [1, "Now I'm opening!"],
 };
 // Enum			{}
-enum EnterID { Customer, System, Viewer }
+enum EnterID {
+  Customer,
+  System,
+  Viewer,
+}
+enum CustomerLv {
+  "A" = "Gold",
+  "B" = "Silver",
+  "C" = "Copper",
+  "D" = "Iron",
+}
 
 // 類型(別名)  Type Aliases   type
 // 聯合類型    Union Types    |
-type UserName = { name: string; };
-type UserAge = {age: number | string };
+type UserName = { name: string };
+type UserAge = { age: number | string };
 type User = UserName & UserAge & { school: string };
 function greet(user: User) {
-  console.log('我叫' + user.name + "，今年" + user.age + "歲，就讀" + user.school);
+  console.log("我叫" + user.name + "，今年" + user.age + "歲，就讀" + user.school);
 }
-greet({ name: '小亭', age: 10, school: 'ＯＯ國小' });
+greet({ name: "小亭", age: 10, school: "ＯＯ國小" });
 // function type		void Unknown never
 // Literal Types
 
-
 // 接口       Interfaces     interface
-interface Building { name: string; birth: number; }
+interface Building {
+  name: string;
+  birth: number;
+}
 
 // type 跟 interface 差別：
 //           | 新增/擴展?   | 修改?
 // Type      | &          | 創建後無法更改
 // Interface | extends    | 可向現有界面添加新字段
 type Animal = { name: string };
-type Bear = Animal & { honey: boolean; };
+type Bear = Animal & { honey: boolean };
 
-interface Story { name: string; };
-interface Histery extends Story { truely: boolean; };
+interface Story {
+  name: string;
+}
+interface Histery extends Story {
+  truely: boolean;
+}
 
 // interface Window { title: string; };
 // interface Window { ts: TypeScriptAPI; };
