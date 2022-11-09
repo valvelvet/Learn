a3f08966c610ff5a736c77591e91e32175833ab8
Annie <anniewang@softmobile.com.tw>
2022-11-09 14:09:03 +0800

    Learn-TS-update mindMap

--- src/product.model.ts
+++ src/product.model.ts
@@ -2,6 +2,7 @@
 // ------------------------------------------------
 // 套件：class validate
 // import { ... } from "class-validator";
+declare const someThingGlobalFromOtherJS: string
 
 export class Product {
   constructor(public title: string, public price: number) {}
